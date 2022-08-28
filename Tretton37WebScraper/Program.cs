e1605a7996c21fc3b9e5fe1ef28cc35b3bdeb215
Melih Kizilsu <melihkizilsu@hotmail.com>
2022-08-28 12:14:56 +0200

    Able to create folder structure for main page

--- Tretton37WebScraper/Program.cs
+++ Tretton37WebScraper/Program.cs
@@ -1,3 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using System.Text.RegularExpressions;
 
-Console.WriteLine("Hello, World!");
+var httpClient = new HttpClient();
+var httpResponseMessage = await httpClient.GetAsync("https://tretton37.com");
+var content = await httpResponseMessage.Content.ReadAsStringAsync();
+// var regex = new Regex(@"href=""(/.[^>]+)""\b");
+var regex = new Regex(@"href=""(/[a-zA-Z\S\.]+)""");
+
+var matches = regex.Matches(content);
+
+foreach (Match match in matches)
+{
+    var link = match.Groups[1];
+    var path = link.Value;
+
+    var fileName = Path.GetFileName(link.Value);
+    if (!string.IsNullOrEmpty(fileName))
+    {
+        path = path.Replace($"{fileName}", string.Empty);
+    }
+    
+    Console.WriteLine($"Creating folder: ./tretton37{path}");
+    Directory.CreateDirectory($"./tretton37{path}");
+}
